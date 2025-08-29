a0d6411f277847343743677fcf9a4c2c17aa4582
knottsam <sknott@smchull.org>
2025-08-29 13:09:48 +0100

    Redeployment test

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
       <h1 className="h1">About</h1>
       <p>
   At Nature & Steel Bespoke, we build durable, timeless furniture in small batches — then collaborate with artists to create unique pieces.
-        one-of-one statement pieces. Our mission: elevate street art into everyday living spaces.
+        one-of-one statement pieces of furniture. Our mission: elevate street art into everyday living spaces.
       </p>
     </div>
   )
