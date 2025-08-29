c2bd92e2a4cb960fde120fa6a788c51259ff1dba
knottsam <sknott@smchull.org>
2025-08-29 17:00:55 +0100

    Edited secrets.

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAcx57ikOep6HhtMknq_G4kakMlMwCjugw",
-  authDomain: "nature-and-steel.firebaseapp.com",
-  projectId: "nature-and-steel",
-  storageBucket: "nature-and-steel.firebasestorage.app",
-  messagingSenderId: "998222720496",
-  appId: "1:998222720496:web:535756bea0dd790bcb0135",
-  measurementId: "G-FB5CMER1TH"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
